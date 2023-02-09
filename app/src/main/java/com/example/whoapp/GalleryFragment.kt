447d3b86af5fdbb4b8b4502e1668dc4546c231f1
Guillermo Fernandez <alufer3712@ieselcaminas.org>
2023-02-09 10:10:34 +0100

    Animación terminada

--- app/src/main/java/com/example/whoapp/GalleryFragment.kt
+++ app/src/main/java/com/example/whoapp/GalleryFragment.kt
@@ -1,10 +1,13 @@
 package com.example.menucontabs
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.utils.widget.ImageFilterView
+import androidx.core.app.ActivityOptionsCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
@@ -51,13 +54,30 @@ class GalleryFragment : Fragment() {
         recView.itemAnimator = DefaultItemAnimator()
 
         adaptador.onClick = {
-            // Al clicar sobre el título de la targeta, es decir el nombre, te lleva a otra actividad
+            // Al clicar sobre la tarjeta, transporta el texto y la imagen a otra actividad
+//            val intent = Intent(activity, PictureActivity::class.java)
+//            intent.putExtra("titulo", items[recView.getChildAdapterPosition(it)].titulo)
+//            intent.putExtra("image", items[recView.getChildAdapterPosition(it)].imagen)
+//            startActivity(intent)
 
+
+            // Al clicar sobre la tarjeta, transporta el texto y la imagen a otra actividad con una ANIMACION
             val intent = Intent(activity, PictureActivity::class.java)
-            intent.putExtra("titulo", items[recView.getChildAdapterPosition(it)].titulo)
-            intent.putExtra("image", items[recView.getChildAdapterPosition(it)].imagen)
-            startActivity(intent)
-
+            val title = items[recView.getChildAdapterPosition(it)].titulo
+            val imagen = items[recView.getChildAdapterPosition(it)].imagen
+            val img = it.findViewById<ImageView>(R.id.imagen_imageView)
+            val text = it.findViewById<TextView>(R.id.textView_card)
+            val p1 = androidx.core.util.Pair<View, String>(img, img.transitionName)
+            val p2 = androidx.core.util.Pair<View, String>(text, text.transitionName)
+            // Transicion
+            val options =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(context as Activity, p1,p2)
+                    .toBundle()
+            options?.putInt("image", imagen)
+            options?.putInt("titulo", title)
+            //Añadimos la información al Intent
+            intent.putExtras(options!!)
+            startActivity(intent, options)
 
         }
 
